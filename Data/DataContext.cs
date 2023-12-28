5a649c5d4ad523be58b338594c179ccd9863fe8b
Alienboy755 <117647710+Alienboy755@users.noreply.github.com>
2023-12-28 13:53:37 +0330

    Implementing DataContext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -18,5 +18,39 @@ namespace PokemonReviewApp.Data
         public DbSet<Pokemon> Pokemons { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Reviewer>Reviewers{ get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(p => new { p.PokemonId, p.CategoryId });
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(p => p.PokemonCategories)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(p => p.PokemonCategories)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<PokemonOwner>()
+                .HasKey(p => new {p.PokemonId, p.OwnerId});
+            modelBuilder.Entity<PokemonOwner>()
+            .HasOne(p => p.Pokemon)
+            .WithMany(p => p.PokemonOwners)
+            .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(p => p.PokemonOwners)
+                .HasForeignKey(p => p.OwnerId);
+
+
+        }
+
+
+
+
     }
 }
